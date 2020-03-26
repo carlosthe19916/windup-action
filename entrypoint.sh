7a647331d9ef967f1f68cd2bc5625682d72214e1
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-26 18:00:37 +0100

    log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -l
 
 echo "----------------------------"
-echo "HOME=$HOME"
-ls -l $HOME
-
-echo "----------------------------"
 echo "WINDUP_HOME=$WINDUP_HOME"
 ls -l $WINDUP_HOME
 
 echo "----------------------------"
 echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
 ls -l $GITHUB_WORKSPACE
+
+mkdir $HOME/rhamt
+echo "----------------------------"
+echo "HOME=$HOME"
+ls -l $HOME
 
 $WINDUP_HOME/bin/rhamt-cli --target $1 \
 --input $GITHUB_WORKSPACE \
