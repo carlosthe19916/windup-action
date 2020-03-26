965b9f8b861289181d36f21196b5ea8b83e1d3bb
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-26 19:17:54 +0100

    upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
+output_folder=$HOME/rhamt/$GITHUB_REPOSITORY
+
 $WINDUP_HOME/bin/rhamt-cli --target $1 \
 --input $GITHUB_WORKSPACE \
---output $HOME/rhamt/$GITHUB_REPOSITORY \
+--output $output_folder \
 $2
