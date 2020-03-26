54d0ff684c48dc68bf57a2029348a11a6ff2908e
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-26 19:02:19 +0100

    change output folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh -l
 
-echo "----------------------------"
-echo "WINDUP_HOME=$WINDUP_HOME"
-ls -l $WINDUP_HOME
-
-echo "----------------------------"
-echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
-ls -l $GITHUB_WORKSPACE
-
-echo "----------------------------"
-whoami
-echo "----------------------------"
-
-echo "HOME=$HOME"
-ls -ld $HOME
-
 $WINDUP_HOME/bin/rhamt-cli --target $1 \
 --input $GITHUB_WORKSPACE \
---output $HOME \
+--output $HOME/rhamt/$GITHUB_REPOSITORY \
 $2
