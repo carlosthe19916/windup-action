ed1cf6dfacbbcb1422a981b9b0f050e7389b78cf
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-26 19:25:54 +0100

    output_folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 output_folder=$HOME/rhamt/$GITHUB_REPOSITORY
+echo "::set-output name=output_folder::$output_folder"
 
 $WINDUP_HOME/bin/rhamt-cli --target $1 \
 --input $GITHUB_WORKSPACE \
