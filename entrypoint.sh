d4696a49b31089fec8dd0c4bb3256aba07edeed5
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-04-12 09:50:07 +0200

    remove sourceMode

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,12 @@ input=VAR1="${2:-$defaultInput}"
 defaultOutput=${GITHUB_WORKSPACE}.report
 output=VAR1="${3:-$defaultOutput}"
 
-sourceMode="${4:-true}"
-
 additionalCommandLineArguments=$4
 
 # Execute RHAMT Analysis
 $WINDUP_HOME/bin/rhamt-cli --target $target \
 --input $input \
 --output $output \
---sourceMode $sourceMode \
 "$additionalCommandLineArguments"
 
 tar -czvf rhamt.tgz $output
