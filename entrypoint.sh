d696fe47cb77b98e3b8f95b40cf1287fceebbd1f
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-04-12 09:08:10 +0200

    update docs and add arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,24 @@
 #!/bin/sh -l
-output=${GITHUB_WORKSPACE}.report
 
-$WINDUP_HOME/bin/rhamt-cli --target $1 \
---input $GITHUB_WORKSPACE \
+target=$1
+
+defaultInput=$GITHUB_WORKSPACE
+input=VAR1="${$2:-$defaultInput}"
+
+defaultOutput=${GITHUB_WORKSPACE}.report
+output=VAR1="${$3:-$defaultOutput}"
+
+sourceMode="${$4:-true}"
+
+additionalCommandLineArguments=$4
+
+# Execute RHAMT Analysis
+$WINDUP_HOME/bin/rhamt-cli --target $target \
+--input $input \
 --output $output \
-$2
+--sourceMode $sourceMode \
+"$additionalCommandLineArguments"
 
 tar -czvf rhamt.tgz $output
-
 report=rhamt.tgz
 echo "::set-output name=report::$report"
