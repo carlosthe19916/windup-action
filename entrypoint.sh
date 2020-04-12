09cb937f6b58f8c5c515f2e591c38f1dbd9d608a
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-04-12 10:48:52 +0200

    remove full path from tar files

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,6 @@ $WINDUP_HOME/bin/rhamt-cli --target $target \
 --output "$output" \
 "$additionalCommandLineArguments"
 
-tar -czvf rhamt.tgz -C $output rhamt
+tar -czvf rhamt.tgz -C $output .
 report=rhamt.tgz
 echo "::set-output name=report::$report"
